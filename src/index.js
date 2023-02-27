cf2b9c66f62d74f434695aae9983a7c61c8fbdf6
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-27 18:15:55 +0200

    Complete

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const searchInput = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
 
 searchInput.addEventListener('input', debounce((e) => {
     const countryName = e.target.value.trim()
 
     if (countryName !== "") {
         list.innerHTML = "";
+        info.innerHTML = "";
         fetchCountries(countryName);
     };
 }, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -13,13 +13,17 @@ export const fetchCountries = (name) => {
         Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
         return;
       };
+
       if (2 <= data.length && data.length <= 10) {
         const countries = renderCountries(data);
         const list = document.querySelector('.country-list');
         list.append(...countries);
         return;
       };
-      
+
+      const countryCard = renderCountry(data);
+      const countryInfo = document.querySelector('.country-info');
+      countryInfo.append(...countryCard);
     })
     .catch(error => {
       console.log(error);
@@ -44,17 +48,45 @@ const renderCountries = (countries) => {
 };
 
 const renderCountry = (country) => {
+  const { name, flags, capital, population, languages } = country[0];
+
   const nameCountry = document.createElement('p');
-  nameCountry.classList.add('country-list__name')
-  nameCountry.textContent = `${country[0].name.common}`;
+  nameCountry.classList.add('country-info__name')
+  nameCountry.textContent = name.common;
   const flag = document.createElement('img');
-  flag.setAttribute('src', `${country[0].flags.svg}`);
-  flag.setAttribute('width', '40');
-  flag.setAttribute('height', '27');
+  flag.setAttribute('src', `${flags.svg}`);
+  flag.setAttribute('width', '80');
+  flag.setAttribute('height', '54');
   nameCountry.prepend(flag);
-  const capital = document.createElement('p');
-  capital.textContent = 'Capital:';
+
+  const capitalStr = document.createElement('p');
+  capitalStr.classList.add('country-info__desc');
+  capitalStr.textContent = capital;
   const capitalValue = document.createElement('span');
-  capitalValue.textContent = `${country[0].capital}`;
-  
+  capitalValue.classList.add('country-info__value');
+  capitalValue.textContent = 'Capital:';
+  capitalStr.prepend(capitalValue);
+
+  const populationStr = document.createElement('p');
+  populationStr.classList.add('country-info__desc');
+  populationStr.textContent = population;
+  const populationValue = document.createElement('span');
+  populationValue.classList.add('country-info__value');
+  populationValue.textContent = 'Population:';
+  populationStr.prepend(populationValue);
+
+  const languagesStr = document.createElement('p');
+  languagesStr.classList.add('country-info__desc');
+  let lang = [];
+  for (const key in languages) {
+    lang.push(languages[key]);
+  };
+  languagesStr.textContent = lang.join(', ');
+  const languagesValue = document.createElement('span');
+  languagesValue.classList.add('country-info__value');
+  languagesValue.textContent = 'Languages:';
+  languagesStr.prepend(languagesValue);
+
+  return [nameCountry, capitalStr, populationStr, languagesStr];
 };
+
