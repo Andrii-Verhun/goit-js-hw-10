6bd1fb337e55e69358925b9ce474fa1c5b5851e8
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-28 15:00:53 +0200

    Fix #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 const debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
@@ -13,6 +14,52 @@ searchInput.addEventListener('input', debounce((e) => {
     if (countryName !== "") {
         list.innerHTML = "";
         info.innerHTML = "";
-        fetchCountries(countryName);
+        fetchCountries(countryName).then(response => {
+            if (!response.ok) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+            }
+            return response.json();
+                })
+        .then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+                return;
+            };
+
+            if (2 <= data.length && data.length <= 10) {
+                renderCountries(data);
+                return;
+            };
+
+            renderCountry(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });;
     };
 }, DEBOUNCE_DELAY));
+
+const renderCountries = (countries) => {
+  const countryList = document.querySelector('.country-list');
+  const listItems = countries.reduce((acc, country) => {
+    return acc += `<li class="country-list__item">
+            <img src="${country.flags.svg}" width="40" height="27">
+            <p class="country-list__name">${country.name.common}</p>
+            </li>`;
+  }, "");
+  countryList.innerHTML = listItems;
+};
+
+const renderCountry = (country) => {
+  const countryInfo = document.querySelector('.country-info');
+  const { name, flags, capital, population, languages } = country[0];
+  const langs = [];
+  for (const key in languages) {
+    langs.push(languages[key]);
+  };
+  countryInfo.innerHTML = `
+  <p class="country-info__name"><img src="${flags.svg}" width="80" height="54">${name.common}</p>
+  <p class="country-info__desc"><span class="country-info--bold">Capital:</span>${capital}</p>
+  <p class="country-info__desc"><span class="country-info--bold">Population:</span>${population}</p>
+  <p class="country-info__desc"><span class="country-info--bold">Languages:</span>${langs.join(', ')}</p>`;
+};
